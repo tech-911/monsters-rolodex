46a85a5ee8f5775ab0ee0d53ef2ecae5db6c920f
tech-911 <babatundeolatunji74@gmail.com>
2021-07-23 01:10:20 +0100

    Updates

---
+++ precache-manifest.df8a6266920f4424cc67f8ce8bfef901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ea3c7b8187163847b9e61cac6f015a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1fa8bfce636935d439a",
+    "url": "/monsters-rolodex/static/css/main.42781723.chunk.css"
+  },
+  {
+    "revision": "aa059d81b5630d669cd2",
+    "url": "/monsters-rolodex/static/js/2.deb0ec8c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.deb0ec8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1fa8bfce636935d439a",
+    "url": "/monsters-rolodex/static/js/main.8f3a3f4e.chunk.js"
+  },
+  {
+    "revision": "b29fd72a6f56199243a3",
+    "url": "/monsters-rolodex/static/js/runtime-main.11416bd7.js"
+  }
+]);
